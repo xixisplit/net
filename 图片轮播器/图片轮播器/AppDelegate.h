c71a70e2338aa12cda696dfa9a7dcfbf0d82afb0
陈曦 <420194918@qq.com>
2016-04-14 17:30:02 +0800

    新建项目.导入素材

---
+++ 图片轮播器/图片轮播器/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  图片轮播器
+//
+//  Created by 陈曦 on 16/4/14.
+//  Copyright © 2016年 xi chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 图片轮播器/图片轮播器/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  图片轮播器
+//
+//  Created by 陈曦 on 16/4/14.
+//  Copyright © 2016年 xi chen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
